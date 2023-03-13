3e2ef88d6709b54053baad2a23c72cda4e5e1ed6
DanielRenaudMN <danielrenaud.dev.mn@gmail.com>
2023-03-13 14:19:45 -0500

    initial setup

---
+++ src/main/java/com/scrounger_api/ScroungerApiApplication.java
@@ -0,0 +1,13 @@
+package com.scrounger_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScroungerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScroungerApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/scrounger_api/ScroungerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scrounger_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScroungerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
